01ae5f013e4468664db5904d884a5afac15bbfa2
xiaobei56 <xiaobei_63@163.com>
2020-07-02 17:44:29 +0800

    FG

--- app/src/main/java/cn/gridlife/lc/kt/MainKT.kt
+++ app/src/main/java/cn/gridlife/lc/kt/MainKT.kt
@@ -11,8 +11,11 @@ package cn.gridlife.lc.kt
 object MainKT {
     @JvmStatic
     fun main(args: Array<String>) {
-        var fg = FG();
+//        var fg = FG();
 //        print(0.toString())
-        print(fg.addBinary("10", "11"))
+//        print(fg.addBinary("10", "11"))
+        var bzi =BZI()
+        var nums:IntArray= intArrayOf(1,2,3,4,5)
+        print(bzi.minSubArrayLen(7,nums))
     }
 }
